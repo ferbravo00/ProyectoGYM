eea1fda04f8bd458d31ed4f34dfa222d2362155c
ferbravo00 <fer.bp001@gmail.com>
2022-11-14 12:51:31 +0100

    Update Gymbros.java

--- proyectGYM/src/main/java/dominio/Gymbros.java
+++ proyectGYM/src/main/java/dominio/Gymbros.java
@@ -10,5 +10,32 @@ package dominio;
  * @author Alumno Mañana
  */
 public class Gymbros {
+    private int idUsuario;
+    private int idUsuarioAmigo;
+
+    public Gymbros(int idUsuario, int idUsuarioAmigo) {
+        this.idUsuario = idUsuario;
+        this.idUsuarioAmigo = idUsuarioAmigo;
+    }
+
+    
+    
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdUsuarioAmigo() {
+        return idUsuarioAmigo;
+    }
+
+    public void setIdUsuarioAmigo(int idUsuarioAmigo) {
+        this.idUsuarioAmigo = idUsuarioAmigo;
+    }
+    
+    
     
 }
