958e73f642de0638bd5f8df550662c661405719b
ferbravo00 <fer.bp001@gmail.com>
2022-11-16 13:06:26 +0100

    Tabla ejercicioDao

--- proyectGYM/src/main/java/com/mycompany/proyectgym/TestMysql.java
+++ proyectGYM/src/main/java/com/mycompany/proyectgym/TestMysql.java
@@ -5,9 +5,11 @@
  */
 package com.mycompany.proyectgym;
 
+import datos.EjercicioDao;
 import datos.UsuarioDao;
 import dominio.Gymbros;
 import datos.GymbrosDao;
+import dominio.Ejercicio;
 import dominio.Usuario;
 import java.sql.Date;
 import java.sql.SQLException;
@@ -26,6 +28,7 @@ public class TestMysql {
         
         UsuarioDao usuarioDao = new UsuarioDao();
         GymbrosDao gymbrosDao = new GymbrosDao();
+        EjercicioDao ejercicioDao = new EjercicioDao();
         
         Usuario u1 = new Usuario ("Fer", "fer@gmail.com", "contraseña", "centro del agua", 22, 176, 72, "foto");
         Usuario u2 = new Usuario ("Jon", "Jon@gmail.com", "contraseña", "elite", 22, 176, 72, "foto");
@@ -33,12 +36,16 @@ public class TestMysql {
         Usuario u4 = new Usuario (7);
         Gymbros g1 = new Gymbros (8,9);
         
-       
-        
+        Ejercicio e1 = new Ejercicio ("Remo unilateral", "Foto", "Este es un ejercicio para trabajar la unilateralidad", "Espalda");
+        Ejercicio e2 = new Ejercicio ("Remo en punta", "Foto", "Este es un ejercicio para trabajar la unilateralidad", "Espalda");
+        Ejercicio e3 = new Ejercicio ("Press banca", "Foto", "Este es un ejercicio para trabajar la unilateralidad", "Pecho");
+        Ejercicio e4 = new Ejercicio ("Dominadas", "Foto", "Este es un ejercicio para trabajar la unilateralidad", "Espalda");
+        Ejercicio e5 = new Ejercicio ("Sentadillas", "Foto", "Este es un ejercicio para trabajar la unilateralidad", "Pierna");
+        Ejercicio e = new Ejercicio ("Espalda");
             //System.out.println(u1);
             //usuarioDao.eliminar(u1);
             //usuarioDao.actualizar(u4);
-            //usuarioDao.insertar(u2);
+            //ejercicioDao.insertar(e5);
             //usuarioDao.insertar(u3);
             
   
@@ -65,6 +72,16 @@ public class TestMysql {
             for (int i = 0; i < gymbro.size(); i++) {
                 System.out.println(gymbro.get(i));
             }*/
+            
+            List<Ejercicio> ejercicio = ejercicioDao.mostrar();
+            ejercicio.forEach(u ->{
+                System.out.println("Ejercicio: "+u);
+            });
+            
+            List<Ejercicio> ejercicios = ejercicioDao.mostrarCuerpo(e);
+            ejercicios.forEach(u ->{
+                System.out.println(u.getParteCuerpo()+": "+u.getNombre()+", "+u.getDescripcion()+", "+u.getFoto());
+            });
             
         } catch (SQLException ex) {
             Logger.getLogger(TestMysql.class.getName()).log(Level.SEVERE, null, ex);
